1829121f7deb0864b24c0fb16c7334f99888a87b
Aman Gupta <aman97703@gmail.com>
2024-08-15 17:53:39 +0530

    update prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { NEXT_AUTH } from "@/lib/auth";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 
-const handler = NextAuth(NEXT_AUTH)
+const handler = NextAuth(NEXT_AUTH as NextAuthOptions)
 
 export { handler as GET, handler as POST };
